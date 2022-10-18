3683d6a93b99d6683c2970baa9320f68c975697b
mikess840 <mike.dynojet@gmail.com>
2022-10-18 22:57:18 +0500

    Rebase check

--- src/main/java/Homework.java
+++ src/main/java/Homework.java
@@ -3,7 +3,7 @@
 public class Homework {
 
     public static void main (String[] args) {
-        Cat markiz = new Cat(8, "Markiz", true);
+        Cat markiz = new Cat(7, "Markiz", false);
         markiz.howOldCat();
         markiz.sayName();
         markiz.isHappy();
